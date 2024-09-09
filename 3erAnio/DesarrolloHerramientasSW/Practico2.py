365c0514a8314405f3d1dd3fbf8f11749ea64deb
Bautista Macedo Rodriguez <116391837+bautimacedo@users.noreply.github.com>
2024-09-08 22:37:26 -0300

    Update Practico2.py

--- 3erAnio/DesarrolloHerramientasSW/Practico2.py
+++ 3erAnio/DesarrolloHerramientasSW/Practico2.py
@@ -1,3 +1,5 @@
+import datetime
+
 #El archivo adjunto contiene temperaturas máximas y mínimas de los últimos 365 días para diferentes estaciones meteorológicas del Servicio
 # Meteorológico Nacional (SMN). Construya un programa en Python que lee los datos del archivo y los coloca en un diccionario cuyas claves son
 # los nombres de las estaciones meteorológicas. El valor asociado a cada clave debe ser otro diccionario con claves "tmax" y "tmin" que mapean con una lista de temperaturas.
@@ -19,16 +21,28 @@ def contar_esp(cadena,lista):
     longitud = len(lista[0])
     return index-longitud
 
+def convertir_fecha(fecha_str):
+    return datetime.datetime.strptime(fecha_str, "%d%m%Y").date()
 
+def obtener_temperatura(estacion, tipo, dias_atras):
+    fecha_actual = datetime.date.today()
+    fecha_deseada = fecha_actual - datetime.timedelta(days=dias_atras)
+    if estacion in estaciones and fecha_deseada in estaciones[estacion][tipo]:
+        return estaciones[estacion][tipo][fecha_deseada]
+    else:
+        return "No hay datos para esa fecha"
 
 estaciones= {}
 archivo = open("C:/Users/bauti/OneDrive/Desktop/IUA/3er Año/DHS/Registro_temperaturas-02092024/temperaturas.txt","r")
 for linea in archivo:
     partes = linea.strip().split()
 
+    fecha_string = partes[0]
+    fecha = convertir_fecha(fecha_string)
+
     if es_numero(partes[1]) and es_numero(partes[2]): #estan las dos temperaturas
-        tmax=partes[1]
-        tmin=partes[2]
+        tmax=float(partes[1])
+        tmin=float(partes[2])
         nombre=" ".join(partes[3:]) # uso [3:] porque puede tener dos palabras el nombre entonces lo une
     elif not es_numero(partes[1]): #no esta ninguna de las dos temperaturas
         tmax=None
@@ -37,17 +51,19 @@ for linea in archivo:
     else: #falta solo una
         if contar_esp(linea,partes)<2:#falta tmin
             tmin=None
-            tmax=partes[1]
+            tmax=float(partes[1])
             nombre=" ".join(partes[2:])
-        elif contar_esp(linea,partes)>2: #falta tmax
+        elif contar_esp(linea,partes)>4: #falta tmax
             tmax=None
-            tmin=partes[1]
+            tmin=float(partes[1])
             nombre = " ".join(partes[2:])
 
     if nombre not in estaciones:  # Cuando no esta el nombre de la estacion, lo agrega como CLAVE, y el VALOR es un diccionario que tiene como clave tmax y tmin y los valores son listas
-        estaciones[nombre] = {"tmax": [], "tmin": []}
-    estaciones[nombre]["tmax"].append(tmax)
-    estaciones[nombre]["tmin"].append(tmin)
+        estaciones[nombre] = {"tmax": {}, "tmin": {}}
 
-dato = estaciones["BUENOS AIRES OBSERVATORIO"]["tmin"]
+    estaciones[nombre]["tmax"][fecha]=tmax
+    estaciones[nombre]["tmin"][fecha]=tmin
+
+#ejemplo
+dato = obtener_temperatura("ROSARIO AERO", "tmin", 22)
 print(dato)
